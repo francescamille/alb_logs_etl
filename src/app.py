b6fcb86f8e18c110b9e3873d1620d71302f66847
Frances Camille Ventocilla <frances.ventocilla@gmail.com>
2022-09-27 22:02:45 +0800

    Load Alpha-2 country codes

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from user_agents import parse
 import re
 import pandas as pd
 
+# Load country codes with Alpha-2 code
+country_codes = pd.read_csv("https://gist.githubusercontent.com/metal3d/5b925077e66194551df949de64e910f6/raw/c5f20a037409d96958553e2eb6b8251265c6fd63/country-coord.csv")
+country_codes.set_index("Country", inplace=True)
+
 def extract(file_path):
     fields = [
         "type",
